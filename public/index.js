0082c598a6db31cc81bca9091138d0d32c55e9e4
deankp <dperez0689@gmail.com>
2022-05-03 19:32:50 -0500

    added server settings

--- public/index.js
+++ public/index.js
@@ -1,14 +1,12 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
     const response = await fetch('http://localhost:9001/counter');
     const result = await response.json();
-
-    console.log(result);
     
-    let countValue = 0;
+    let countValue = result.value;
 
     function increment(){
         countValue++;
